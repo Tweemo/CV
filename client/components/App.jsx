8057fe5db1a71859a8f5db694c987e514c863f05
Tim Liew <74855211+Tweemo@users.noreply.github.com>
2022-04-25 16:48:45 +1200

    added random css need to add details

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,11 +4,36 @@ import InfoBox from './infoBox'
 const App = () => {
   
   return (
-    <div>
-      <InfoBox category="Experience"/>
-      <InfoBox category="Experience"/>
-      <InfoBox category="Experience"/>
+    <>
+    <div className='content-container'>
+      <div className='main-title'>
+        <h1>Hi, My Name Jeff</h1>
+      </div>
+      <div className='content'>
+        <InfoBox category="Experience"/>
+        <InfoBox category="Project"/>
+        <InfoBox category="Dunno"/>
+        <InfoBox category="Experience"/>
+        <InfoBox category="Project"/>
+        <InfoBox category="Dunno"/>
+        <InfoBox category="Experience"/>
+        <InfoBox category="Project"/>
+        <InfoBox category="Dunno"/>
+        <InfoBox category="Experience"/>
+        <InfoBox category="Project"/>
+        <InfoBox category="Dunno"/>
+        <InfoBox category="Experience"/>
+        <InfoBox category="Project"/>
+        <InfoBox category="Dunno"/>
+        <InfoBox category="Experience"/>
+        <InfoBox category="Project"/>
+        <InfoBox category="Dunno"/>
+        <InfoBox category="Experience"/>
+        <InfoBox category="Project"/>
+        <InfoBox category="Dunno"/>
+      </div>
     </div>
+    </>
   )
 }
 
--- client/components/infoBox.jsx
+++ client/components/infoBox.jsx
@@ -10,13 +10,17 @@ const InfoBox = (props) => {
 
   return (
     <div onClick={dropDaBox} className='dropbox'>
-      {/* Some sort of name of each category */}
       <div className='boxName'>
         {props.category}
       </div>
-      <div className='expand-btn'>
-      {isVisible ? '-' : '+'}
-      </div>
+      <>
+        <div className='expand-btn'>
+        {isVisible ? '-' : '+'}
+        </div>
+        <div>
+        {/* Insert Some function of way the info gets plonked. Likely function from App that sends it through props to here. dunno yet */}
+        </div>
+      </>
     </div>
   )
 }
